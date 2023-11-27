516f80d55b58c11e39ec32aaa6c07e43443185fd
= <=>
2023-11-27 21:22:34 +0300

    feedback form updated

---
+++ src/components/shared/BaseRadio.vue
@@ -0,0 +1,31 @@
+<template>
+  <input
+    type="radio"
+    :checked="modelValue === value"
+    :value="value"
+    v-bind="$attrs"
+    @change="$emit('update:modelValue', value)"
+  />
+  <label
+    v-if="label"
+    class="ml-2 text-sm relative -top-[2px]"
+    >{{ label }}</label
+  >
+</template>
+
+<script setup>
+defineProps({
+  label: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: [String, Number],
+      default: ''
+    },
+    value: {
+      type: [String, Number],
+      required: true
+    },
+})
+</script>
